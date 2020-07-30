c9513ec7d0b80bee90685e200ae6b405d2a9c495
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-07-30 17:39:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-def mario(phrase)
-  puts phrase = "It's-a me, Mario!" 
+def mario
+  puts "It's-a me, Mario!" 
 end
-mario(phrase) 
+
 
 def toadstool(status)
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
